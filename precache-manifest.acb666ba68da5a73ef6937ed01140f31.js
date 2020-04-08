1f7fee11b6a0d0adb6505e5ad692f48212cafaba
robinlin1973 <jielin88@hotmail.com>
2020-04-07 21:46:40 -0400

    Updates

---
+++ precache-manifest.acb666ba68da5a73ef6937ed01140f31.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f2c8bdbae479e121269e9882a4b2808",
+    "url": "/coronadashboard/index.html"
+  },
+  {
+    "revision": "f1bae413668e3a647948",
+    "url": "/coronadashboard/static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "f1a1cf6e54c0949bd812",
+    "url": "/coronadashboard/static/css/main.3340609c.chunk.css"
+  },
+  {
+    "revision": "f1bae413668e3a647948",
+    "url": "/coronadashboard/static/js/2.7de59133.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/coronadashboard/static/js/2.7de59133.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1a1cf6e54c0949bd812",
+    "url": "/coronadashboard/static/js/main.eef8ab02.chunk.js"
+  },
+  {
+    "revision": "c3fe8b715f1cba43cf22",
+    "url": "/coronadashboard/static/js/runtime-main.da090dc2.js"
+  },
+  {
+    "revision": "f27c17a41c84a40f89a2b64fec44c19b",
+    "url": "/coronadashboard/static/media/covid.f27c17a4.csv"
+  }
+]);
